16283e54f5712a1e1d6460e46081e5d2c59a5a35
Jairo Rondon <jimnoname@gmail.com>
2020-07-22 13:19:45 -0500

    add ssr

---
+++ gatsby-ssr.js
@@ -0,0 +1,15 @@
+import React from "react";
+export const onReadyBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      data-ad-client="ca-pub-5224853986664567"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>,
+    <script
+      type="text/javascript"
+      src="https://platform-api.sharethis.com/js/sharethis.js#property=5f187efb07173c00125314c9&product=inline-share-buttons&cms=sop"
+      async="async"
+    ></script>,
+  ]);
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,14 +6,12 @@ import Footer from "./footer";
 import SEO from './seo'
 import "../css/style.css";
 
-const Layout = ({ headerClass, children, title, isInternal }) => {
+const Layout = ({ headerClass, children, title, isInternal, ...props }) => {
   
   return (
     <>
-      <SEO title={title}/>
+      <SEO title={title} {...props}/>
       <Helmet>
-        <script data-ad-client="ca-pub-5224853986664567" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=5f187efb07173c00125314c9&product=inline-share-buttons&cms=sop' async='async'></script>
         <meta name="google-site-verification" content="tNgg_NYBUoKEukRp7rQcwCPqFoLYl9mg5JBz9ZgChyg" />
         <body className="font-sans antialiased bg-gray-100" />
       </Helmet>
